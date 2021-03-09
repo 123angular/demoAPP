3a0af9042ed34e3498776feddbd2629db94d268e
angular <angular@gmail.com>
2021-03-09 17:17:50 +0530

    - changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      Jaimisha
-    </div>
-=======
    <h1>Dhruvi</h1>
->>>>>>> ed22b25c9ca2b31481653cea2bd71187763d3ecd
   );
 }
 
